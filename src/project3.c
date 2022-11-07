e089196cb4c516efa385d91fb6f53d22f9d11672
sebub <105357150+sebub@users.noreply.github.com>
2022-11-07 21:19:57 +0100

    Update project3.c

--- src/project3.c
+++ src/project3.c
@@ -1,6 +1,25 @@
+#include <stdio.h>
 
+typedef struct{
+    char*   weekday;
+    int     date;
+    double  time;
+    char*   teams;
+    char*   result;
+    int     spectators;
+}gamesPlayed;
+
+typedef struct{
+    char*   team;
+    int     points;
+    int     goals_out;
+    int     goals_in;
+}teams;
 
 int main(void)
 {
+gamesPlayed games_array[132];
+teams teams_array[12];
+
 
 }
