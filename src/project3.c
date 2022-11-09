cf85c78725c9a4c058968eafb80d5deb3a10f328
sebub <105357150+sebub@users.noreply.github.com>
2022-11-09 19:21:25 +0100

    df

--- src/project3.c
+++ src/project3.c
@@ -18,25 +18,41 @@ typedef struct {
     int goals_in;
 } teams;
 
+void fill_games_array(FILE* file ,gamesPlayed *array, int games);
+void print_games_array(gamesPlayed *array, int games);
 
 int main(void) {
     gamesPlayed games_array[132];
     teams teams_array[12];
 
-    FILE *ptr = fopen("kampe-2020-2021.txt", "r");
-    if (ptr == NULL) {
+    FILE *games_played = fopen("kampe-2020-2021.txt", "r");
+    if (games_played == NULL) {
         printf("no such file.");
-        return 0;
     }
 
-    while (fscanf(ptr, "%s %s %lf %s %*s %s %d %*s %d %d",
-                  games_array->weekday, games_array->date, &games_array->time, games_array->team1, games_array->team2,
-                  &games_array->scoreteam1, &games_array->scoreteam2, &games_array->spectators) == 8)
-        printf("%s %s %lf %s %s %d %d %d\n", games_array->weekday, games_array->date, games_array->time,
-               games_array->team1, games_array->team2, games_array->scoreteam1, games_array->scoreteam2,
-               games_array->spectators);
+    fill_games_array(games_played,games_array, 132);
+    print_games_array(games_array, 132);
+
+
     return 0;
 
 
 
+}
+
+void fill_games_array(FILE* file ,gamesPlayed *array, int games){
+    for (int i = 0; i < games; ++i) {
+        fscanf(file, "%s %s %lf %s %*s %s %d %*s %d %d",
+               array[i].weekday, array[i].date, &array[i].time,
+               array[i].team1, array[i].team2, &array[i].scoreteam1, &array[i].scoreteam2,
+               &array[i].spectators);
+    }
+}
+
+void print_games_array(gamesPlayed *array, int games){
+    for (int i = 0; i < games; ++i) {
+        printf("%s %s %lf %s %s %d %d %d\n", array[i].weekday, array[i].date, array[i].time,
+               array[i].team1, array[i].team2, array[i].scoreteam1, array[i].scoreteam2,
+               array[i].spectators);
+    }
 }
