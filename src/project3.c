47d21397de672efc0576d6e5dd9e5d0edf6f4066
sebub <105357150+sebub@users.noreply.github.com>
2022-11-09 16:35:47 +0100

    Update project3.c

--- src/project3.c
+++ src/project3.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 
-typedef struct{
-    char   weekday[10];
-    char     date[6];
-    double  time;
-    char*   teams;
-    char*   result;
-    int     spectators;
-}gamesPlayed;
+typedef struct {
+    char weekday[10];
+    char date[6];
+    double time;
+    char team1[5];
+    char team2[5];
+    int scoreteam1;
+    int scoreteam2;
+    int spectators;
+} gamesPlayed;
 
-typedef struct{
-    char*   team;
-    int     points;
-    int     goals_out;
-    int     goals_in;
-}teams;
+typedef struct {
+    char *team;
+    int points;
+    int goals_out;
+    int goals_in;
+} teams;
 
-int main(void)
-{
-gamesPlayed games_array[132];
-teams teams_array[12];
-    FILE* ptr = fopen("kampe-2020-2021.txt", "r");
+
+int main(void) {
+    gamesPlayed games_array[132];
+    teams teams_array[12];
+
+    FILE *ptr = fopen("kampe-2020-2021.txt", "r");
     if (ptr == NULL) {
         printf("no such file.");
         return 0;
     }
 
-    /* Assuming that test.txt has content
-       in below format
-    NAME AGE CITY
-    abc     12 hyderbad
-    bef     25 delhi
-    cce     65 bangalore */
-    char buf[100];
-    while (fscanf(ptr, "%s %s %lf %*s %*s %*s %*s %*s %*s %*s", games_array->weekday,games_array->date, &games_array->time, games_array->teams)== 3)
-        printf("%s %s %lf\n",games_array->weekday,games_array->date,games_array->time);
+    while (fscanf(ptr, "%s %s %lf %s %*s %s %d %*s %d %d",
+                  games_array->weekday, games_array->date, &games_array->time, games_array->team1, games_array->team2,
+                  &games_array->scoreteam1, &games_array->scoreteam2, &games_array->spectators) == 8)
+        printf("%s %s %lf %s %s %d %d %d\n", games_array->weekday, games_array->date, games_array->time,
+               games_array->team1, games_array->team2, games_array->scoreteam1, games_array->scoreteam2,
+               games_array->spectators);
     return 0;
+
+
 
 }
