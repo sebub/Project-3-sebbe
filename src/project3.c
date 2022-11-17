c761682528dcc7bd8774cb5d77e5f0e18c5cdea3
sebub <105357150+sebub@users.noreply.github.com>
2022-11-17 22:21:50 +0100

    Update project3.c

--- src/project3.c
+++ src/project3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <search.h>
+#include <stdlib.h>
 
 #define numberofgames 132
 #define numberofteams 12
@@ -25,33 +26,65 @@ typedef struct {
     int goal_dif;
 } teams;
 
-
+/***
+ *Transfers data about football games from txt file to an array.
+ * @param file Takes file with game data
+ * @param array Array of type gamesPlayed to be filled with the data from the file
+ * @param games Number of games contained in the passed file
+ */
 void fill_games_array(FILE *file, gamesPlayed *array, int games);
 
-void fill_teams_array(gamesPlayed *games_array, teams *teams_array);
-
+/***
+ * Assigns each index a team name and sets the values of goals_out, goals_in, goal_dif and points to 0.
+ * @param teams_array Array of struct teams
+ * @param team_names Array of team names
+ */
 void reset_team_array(teams *teams_array, char **team_names);
 
-void print_teams_array(teams *array, int games);
+/***
+ * Transfers from games_array to teams_array using a hash function and calculates points and score difference
+ * @param games_array Array filled with data about games
+ * @param teams_array Array with teams, points and score
+ */
+void fill_teams_array(gamesPlayed *games_array, teams *teams_array);
 
-int team_array_sort_logic(const void *a, const void *b);
+/***
+ * Prints an array of struct teams
+ * @param array Array of struct teams
+ * @param teams Number of teams in the array
+ */
+void print_teams_array(teams *array, int teams);
 
 void sort_team_array(teams *array);
 
+/***
+ * Logic for sorting team_array, sorts by points, then by score difference
+ * @param a
+ * @param b
+ * @return
+ */
+int team_array_sort_logic(const void *a, const void *b);
+
+/***
+ * enables us to call an index in out team_array by a name in stead of index number
+ * @param name
+ * @return
+ */
 int team_name_hash(char *name);
 
 int main(void) {
     char *team_names[numberofteams] = {"FCK", "SDR", "ACH", "AaB", "LBK", "RFC",
                                        "BIF", "FCM", "AGF", "VB", "FCN", "OB"};
     gamesPlayed games_array[numberofgames];
-    teams teams_array[12];
+    teams teams_array[numberofteams];
 
-    FILE *games_played = fopen("kampe-2020-2021.txt", "r");
-    if (games_played == NULL) {
+    FILE *games_played_data = fopen("kampe-2020-2021.txt", "r");
+    if (games_played_data == NULL) {//If file does not exist exit program
         printf("no such file.");
+        exit(0);
     }
 
-    fill_games_array(games_played, games_array, numberofgames);
+    fill_games_array(games_played_data, games_array, numberofgames);
 
     reset_team_array(teams_array, team_names);
     print_teams_array(teams_array, numberofteams);
@@ -76,11 +109,12 @@ void fill_games_array(FILE *file, gamesPlayed *array, int games) {
                array[i].team1, array[i].team2, &array[i].scoreteam1, &array[i].scoreteam2,
                &array[i].spectators);
     }
+    fclose(file);
 }
 
-void print_teams_array(teams *array, int games) {
+void print_teams_array(teams *array, int teams) {
     printf(" Team    Points   Score\n");
-    for (int i = 0; i < games; ++i) {
+    for (int i = 0; i < teams; ++i) {
         printf("| %3s %5d %8d - %d |\n", array[i].name, array[i].points, array[i].goals_out, array[i].goals_in);
     }
 
